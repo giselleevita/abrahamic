ae2e51f7c1e9122817fea23cbad0f090d66c2cba
MrClue <Motar19@student.sdu.dk>
2026-05-13 12:28:17 +0200

    test: add test route to verify route system

---
+++ src/app/test-route/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div className="p-12">
+      <h1>Test Route Working</h1>
+      <p>If you see this, the route system is working properly.</p>
+    </div>
+  )
+}
